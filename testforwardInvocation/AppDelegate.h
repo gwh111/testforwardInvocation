6dfbe23151d64226eac5fdb297e9d6313b2e0bf5
gwh111 <173695508@qq.com>
2018-05-22 11:25:10 +0800

    Initial Commit

---
+++ testforwardInvocation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testforwardInvocation
+//
+//  Created by gwh on 2018/5/22.
+//  Copyright © 2018年 gwh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
